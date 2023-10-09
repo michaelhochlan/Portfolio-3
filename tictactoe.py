812d9f84dd5121422bf82a97818bd45c608dfd4f
Michael Hochlan <michael.hochlan@gmail.com>
2023-10-09 12:34:39 +0200

    Add Steps for creating a TicTacToe game as comments

---
+++ tictactoe.py
@@ -0,0 +1,8 @@
+# To do list for a Tic Tac Toe game
+#	1. Create the board
+#	2. Take first player input
+#	3. Check for win or tie
+#	4. Take second player input
+#	5. Check again for win or tie
+#	6. Go back to 1.
+
