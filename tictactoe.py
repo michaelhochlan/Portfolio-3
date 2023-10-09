71df8f6d7683caa5139f94685dbb4da4fc94a936
Michael Hochlan <michael.hochlan@gmail.com>
2023-10-09 12:53:47 +0200

    Add a global variable as list with the game fields 1 to 9 and add the function to draw the board

--- tictactoe.py
+++ tictactoe.py
@@ -6,3 +6,17 @@
 #	5. Check again for win or tie
 #	6. Go back to 1.
 
+# Define global functions for the game
+
+board = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
+
+def printBoard(board):
+    print(board[0] + "  |  " + board[1] + "  |  " + board[2])
+    print("--------------")
+    print(board[3] + "  |  " + board[4] + "  |  " + board[5])
+    print("--------------")
+    print(board[6] + "  |  " + board[7] + "  |  " + board[8])
+    
+printBoard(board)
